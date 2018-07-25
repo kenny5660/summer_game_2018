b58a81a4a56d662a245acc492856cd66dcdf9fbb
Михаил Лямаев <misha25_live@mail.ru>
2018-07-25 13:35:10 +1000

    удаллил коменты

--- sum_game_2018/sum_game_2018/app.ts
+++ sum_game_2018/sum_game_2018/app.ts
@@ -15,17 +15,10 @@ function ready() {
         player.keyup(e);
     }
   
-    // resize the canvas to fill browser window dynamically
     window.addEventListener('resize', resizeCanvas, false);
-
     function resizeCanvas() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-
-        /**
-         * Your drawings need to be inside this function otherwise they will be reset when 
-         * you resize the browser window and the canvas goes will be cleared.
-         */
         drawStuff();
     }
     resizeCanvas();
