ce375f217a1f68f5cefad37eb2f5edcae8195104
Михаил Лямаев <misha25_live@mail.ru>
2018-07-27 01:37:56 +1000

    add 20 random eated objects

--- sum_game_2018/sum_game_2018/app.ts
+++ sum_game_2018/sum_game_2018/app.ts
@@ -8,34 +8,23 @@ function ready() {
     var curScene = gameScene;
     var player = new PlayerGameObject(canvas, "green");
     curScene.GameObjects.push(player);
-    //addEventListener("keydown", KeyBoardListener_keydown);
-    //addEventListener("keyup", KeyBoardListener_keyup);
-    //function KeyBoardListener_keydown(e: KeyboardEvent) {
-    //    player.keydown(e);
-    //}
-    //function KeyBoardListener_keyup(e: KeyboardEvent) {
-    //    player.keyup(e);
-    //}
     var mainMenuScene = new Scene(canvas, "grey");
-  //  var curScene = gameScene;
-    var curScene = mainMenuScene;
+    var curScene = gameScene;
+   // var curScene = mainMenuScene;
     var startButton = new MenuButton("START", new Point(300, 300));
     startButton.width = 400;
     startButton.height = 150;
     startButton.fontSize = 5;
     mainMenuScene.GameObjects.push(startButton);
     mainMenuScene.GameObjects.push(new MenuButton("EXIT", new Point(400, 490)));
-    var player = new PlayerGameObject(new Point(30, 30), 20, "green");
+
+    var player = new PlayerGameObject(canvas,"green");
     gameScene.GameObjects.push(player);
-    addEventListener("keydown",KeyBoardListener_keydown);
-    addEventListener("keyup", KeyBoardListener_keyup);
-    function KeyBoardListener_keydown(e: KeyboardEvent) {
-        player.keydown(e);
+
+    for (var i = 0; i < 20; i++) {
+        gameScene.GameObjects.push(new eatedObject(new Point(Math.abs(Math.random() * canvas.width), Math.abs(Math.random() * canvas.height)), GameConfig.eatedSize,"red"));
     }
-    function KeyBoardListener_keyup(e: KeyboardEvent) {
-        player.keyup(e);
-    }
-  
+   
     window.addEventListener('resize', resizeCanvas, false);
     function resizeCanvas() {
         canvas.width = window.innerWidth;
